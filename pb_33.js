21939d0f8a399f56c6097aecb98588ac5f4c9bcb
yash pateliya <pateliyayash5@gmail.com>
2026-03-13 00:36:32 +0530

    33

---
+++ pb_33.js
@@ -0,0 +1,19 @@
+;
+
+
+const example={
+    "str":"This will our JSON file",
+    "date":"2006-2-22"
+}
+
+var d=example.date
+console.log("our date :-" + d)
+var ind_date=new Date(d)  
+console.log("our new date :-" + ind_date)
+
+
+
+// dont forget to add "" in date 
+// class - Date
+//   22 -2 -2006 ERROR
+// 2006-2-22 CORRECT YYYY-MM-DD
