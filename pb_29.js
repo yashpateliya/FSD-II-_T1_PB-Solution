495d601b648913a64b9cec838ef347ba40049745
yash pateliya <pateliyayash5@gmail.com>
2026-03-13 21:22:12 +0530

    3 marks

---
+++ pb_29.js
@@ -0,0 +1,21 @@
+// pb_29.js
+// Write a script to define two JSON objects named as “division1” and
+// “division2” having an array to store 5 names of students along with
+// their roll number and list of subjects they opted.
+
+
+
+const division1 = {
+  names: ["lorem1", "lorem2", "lorem3", "lorem4", "lorem5"],
+  rollNo: [1, 2, 3, 4, 5],
+  subjects: ["FSD-2", "DM", "TOC", "Python-2"]
+};
+
+const division2 = {
+  names: ["lorem6", "lorem7", "lorem8", "lorem9", "lorem10"],
+  rollNo: [6, 7, 8, 9, 10],
+  subjects: ["FSD-2", "DM", "TOC", "Python-2"]
+};
+
+console.log(division1);
+console.log(division2);
