09c3c1d8bcc0d2b352b2641ff3988d8c613f979e
yash pateliya <pateliyayash5@gmail.com>
2026-03-13 22:19:55 +0530

    2-m

---
+++ pb_25.js
@@ -0,0 +1,23 @@
+// Write a script of JSON array containing objects and display the same
+// in console
+
+
+const arr=[
+    {
+        name:"Yash",
+        age:20,
+        city:"Vadodra"
+    },
+    {
+        name:"aBC",
+        age:21,
+        city:"Surat"
+    },
+    {
+        name:"xYZ",
+        age:19,
+        city:"Ahmedabad"
+    }
+]
+
+console.log(arr)
